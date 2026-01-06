420224b8ed6dc4ba9e033fe99c913b1104b52b7c
mavmav <maumandac@gmail.com>
2026-01-06 21:40:31 +1300

    Start first js code; conditionals

--- javascript-start/js-basics/script.js
+++ javascript-start/js-basics/script.js
@@ -1,1 +1,12 @@
 console.log("Hello from our First JS file!")
+
+// Conditionals
+/* if (1 + 1 === 2) {
+    console.log("Math still works!")
+}
+*/ 
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5")
+    console.log(random);
+}
