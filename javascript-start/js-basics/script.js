c9cb76a11306f0c4ab176dff69b4f5f92be751ea
mavmav <maumandac@gmail.com>
2026-01-11 23:36:04 +1300

    Add if statement for greater output

--- javascript-start/js-basics/script.js
+++ javascript-start/js-basics/script.js
@@ -10,3 +10,8 @@ if (random < 0.5) {
     console.log("Your number is less than 0.5")
     console.log(random);
 }
+
+if (random >= 0.5) {
+    console.log("Your number is GREATER than 0.5")
+    console.log(random);
+}
