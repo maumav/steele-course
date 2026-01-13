800affce10068e31484427e7d70c777393028757
mavmav <maumandac@gmail.com>
2026-01-13 17:48:56 +1300

    Add else if sample

--- javascript-start/js-basics/script.js
+++ javascript-start/js-basics/script.js
@@ -5,13 +5,24 @@ console.log("Hello from our First JS file!")
     console.log("Math still works!")
 }
 */ 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5")
-    console.log(random);
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+//     console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("Your number is GREATER than 0.5")
-    console.log(random);
-}
+// if (random >= 0.5) {
+//     console.log("Your number is GREATER than 0.5")
+//     console.log(random);
+// }
+
+/*Using else if */
+// const dayOfWeek = "Monday";
+// if (dayOfWeek === "Monday"){
+//     console.log("I hate Mondays!")
+// } else if (dayOfWeek === "Saturday"){
+//     console.log("I LOVE Saturday!")
+// } else if (dayOfWeek === "Friday"){
+//     console.log("I LOVE Friday JAM JAM!")
+// }
+
