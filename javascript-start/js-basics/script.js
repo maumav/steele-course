0e68b69948bbcf234da945a844278ffb2e6b6b7a
mavmav <maumandac@gmail.com>
2026-01-13 18:10:53 +1300

    Add prompt input for daysOfWeek exercise

--- javascript-start/js-basics/script.js
+++ javascript-start/js-basics/script.js
@@ -9,22 +9,24 @@ console.log("Hello from our First JS file!")
 // if (random < 0.5) {
 //     console.log("Your number is less than 0.5")
 //     console.log(random);
+// } else {
+//     console.log("Your number is GREATER or EQUAL than 0.5")
 // }
 
-// if (random >= 0.5) {
-//     console.log("Your number is GREATER than 0.5")
-//     console.log(random);
-// }
 
 /*Using else if */
-// const dayOfWeek = "Monday";
-// if (dayOfWeek === "Monday"){
+// const dayOfWeek = prompt("Enter a day").toLowerCase();
+// if (dayOfWeek === "monday"){
 //     console.log("I hate Mondays!")
-// } else if (dayOfWeek === "Saturday"){
+// } else if (dayOfWeek === "saturday"){
 //     console.log("I LOVE Saturday!")
-// } else if (dayOfWeek === "Friday"){
+// } else if (dayOfWeek === "friday"){
 //     console.log("I LOVE Friday JAM JAM!")
+// } 
+// else {
+//     console.log("Invalid")
 // }
+
 
 /*Fair Entry Price*/
 // 0-5 FREE
@@ -32,13 +34,15 @@ console.log("Hello from our First JS file!")
 // 10 - 65 $20
 // 65+ Senior $10
 
-// const age = 15;
+const age = 68;
 
-// if (age < 5 ) {
-//     console.log("You are a baby, FREE!")
-// } else if (age < 10 ) {
-//     console.log("You are a child. $10")
-// } else if (age < 65) {
-//     console.log("You are an adult. $20")
-// }
+if (age < 5 ) {
+    console.log("You are a baby, FREE!")
+} else if (age < 10 ) {
+    console.log("You are a child. $10")
+} else if (age < 65) {
+    console.log("You are an adult. $20")
+} else {
+    console.log("You are a Senior. $10")
+}
 
