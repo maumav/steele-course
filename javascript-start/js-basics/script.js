af6551ca49181fb46c9846effe4bf3678c178d15
mavmav <maumandac@gmail.com>
2026-01-14 21:09:09 +1300

    Add example for logical && and ||

--- javascript-start/js-basics/script.js
+++ javascript-start/js-basics/script.js
@@ -34,15 +34,65 @@ console.log("Hello from our First JS file!")
 // 10 - 65 $20
 // 65+ Senior $10
 
-const age = 68;
+// const age = 68;
 
-if (age < 5 ) {
-    console.log("You are a baby, FREE!")
-} else if (age < 10 ) {
-    console.log("You are a child. $10")
-} else if (age < 65) {
-    console.log("You are an adult. $20")
-} else {
-    console.log("You are a Senior. $10")
+// if (age < 5 ) {
+//     console.log("You are a baby, FREE!")
+// } else if (age < 10 ) {
+//     console.log("You are a child. $10")
+// } else if (age < 65) {
+//     console.log("You are an adult. $20")
+// } else {
+//     console.log("You are a Senior. $10")
+// }
+
+/*truthy and falsey */
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("Truthy")
+// }
+// else {
+//     console.log("Falsey")
+// 
+
+/*Logical Operators */
+/* &&*/
+// true && true 
+// true
+
+// true && false 
+// false
+
+// false && false
+// false 
+
+// false && true 
+// false 
+
+    // const password = prompt("Enter a password");
+
+    // if (password.length >= 6 && password.indexOf(' ') === -1) {
+    //     console.log("Valid Password")
+    // }
+    // else {
+    //     console.log("Incorrect format for password.")
+    // }
+
+/*Logical OR */
+
+//  0-5 free
+//  5-10 $10 
+//  10-65 $20 
+//  65+ free 
+const age = 7;
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log("Free");
+} 
+else if (age >= 5 && age < 10) {
+    console.log("$10")
 }
+else if (age >= 10 && age < 65) {
+    console.log("$20")
 
+} 
