3df691cd7036465d3b98e56fc32ce9f70cb4867a
mavmav <maumandac@gmail.com>
2026-02-04 08:32:05 +1300

    Add a way to quit game and fix bugs

--- javascript-start/guessingGame/app.js
+++ javascript-start/guessingGame/app.js
@@ -8,11 +8,17 @@ let attempts = 1;
 
 let guess = parseInt(prompt("Enter your first guess!"));
 while (guess !== targetNum) {
+    if (guess === 'q') breakl;
+    guess = parseInt(guess);
     attempts++;
     if (guess > targetNum) {
         guess = parseInt(prompt("Too high! Enter a new guess: "));
-    } else {
+        attempts;
+    } else if (guess < targetNum){
         guess = parseInt(prompt("Too low! Enter again!"));
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Enter a number or 'q' to quit.")
     }
 }
 console.log(`You got it! It took ${attempts} guesses.`)
