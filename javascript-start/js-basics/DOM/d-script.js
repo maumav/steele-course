701f8a358f6b40fe200f5d4b3163b79e8045aa30
mavmav <maumandac@gmail.com>
2026-04-24 05:26:20 +1200

    Add notes on using dom to manipulate contents

--- javascript-start/js-basics/DOM/d-script.js
+++ javascript-start/js-basics/DOM/d-script.js
@@ -7,5 +7,27 @@ A bunch of ojects that interacts with js.
 type 
 console.dir(document) in the console to see the js document.'
 
+Selecting:
+    - getElementById
+    -getElementsByTagName
+    -getElementsByClassName
 */
+    // const banner = document.getElementById('banner')
+        //save the banner id as banner variable
 
+//query selector
+    //one method to select element name, id or class
+
+document.querySelector('a[title="Java"') 
+    //selecting an achor tag with attribute of Java.
+
+    /*In-browser exercise*/
+
+    const doneTodos = document.querySelectorAll('.done')
+    const checkbox = document.querySelector('[type="checkbox"]')
+
+//MANIPULATING using DOM
+    // document.querySelector('p').innerText = 'new text here'
+    
+    /*In-browser exercise*/
+    //  document.querySelector('span').innerHTML ='Disgusting'
